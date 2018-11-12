7f60bf00e18f92432c2acad4355d728bbcd2b3d8
League Student <league@iMac-9.attlocal.net>
2018-11-11 17:27:39 -0800

    hard work good job
    fix delete and start undo

--- src/_03_IntroToStacks/_02_TextUndoRedo.java
+++ src/_03_IntroToStacks/_02_TextUndoRedo.java
@@ -1,17 +1,35 @@
 package _03_IntroToStacks;
 
+import java.awt.Component;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.util.Stack;
+
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
-public class _02_TextUndoRedo {
+public class _02_TextUndoRedo implements KeyListener {
 	
 	JFrame frame = new JFrame();
 	JPanel panel = new JPanel();
 	JLabel label = new JLabel();
 	
+	Stack<Character> textDelete = new Stack <Character>();
+	public static void main(String[] args) {
+		_02_TextUndoRedo a = new _02_TextUndoRedo();
+	}
 	
+	_02_TextUndoRedo(){
+	frame.add(panel);
+	panel.add(label);
+	label.setText("");
+	frame.addKeyListener(this);
 	
+	frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	frame.setSize(1000, 500);
+	frame.setVisible(true);
+	}
 	/* 
 	 * Create a JFrame with a JPanel and a JLabel.
 	 * 
@@ -24,6 +42,27 @@ public class _02_TextUndoRedo {
 	 * off the Stack and added back to the JLabel.
 	 * 
 	 * */
+
+
+	public void keyPressed(KeyEvent e) {
+		if(e.getKeyCode()==KeyEvent.VK_BACK_SPACE) {
+			String s = label.getText();
+			char echo = s.charAt(s.length()-1);
+			textDelete.push(echo);
+			String serria = s.substring(0, s.length()-1);
+				label.setText(serria);
+				
+			}
+		if(e.getKeyCode()==KeyEvent.VK_DOWN) {
+			
+		}
+		else {
+			label.setText(label.getText()+e.getKeyChar());
+		}
+	}
+		
+	public void keyReleased(KeyEvent e) {}
+	public void keyTyped(KeyEvent e) {}
 	
 	
 }
