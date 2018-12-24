86da7d8ad55fcde43eed3b94e0e85fc613a660e0
League Student <league@iMac-8.attlocal.net>
2018-12-23 17:27:28 -0800

    Fininsh the checkpoints

---
+++ src/Checkpoint.java
@@ -0,0 +1,34 @@
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.Stack;
+
+public class Checkpoint {
+/*		//CHECKPOINT ARRAYLIST
+	public static void main(String[] args) {
+		
+		ArrayList<Cow> a = new ArrayList<Cow>();
+		Cow b = new Cow();
+		a.add(b);
+		int d = 0;
+		Random bob = new Random();
+		d=bob.nextInt(Integer.MAX_VALUE);
+		for (int i = 0; i < d; i++) {
+			a.add(b);
+		}
+		for (int j = 0; j < a.size(); j++) {
+			a.get(j).feed();
+		}
+		
+	}
+	
+}
+
+class Cow {
+    public void feed(){}
+    */
+		//CHECKPOINT STACKS
+	public static void main(String[] args) {
+		Stack<Integer> a = new Stack<Integer>();
+		
+	}
+  }
